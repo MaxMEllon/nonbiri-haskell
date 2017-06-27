02ae4dd9680ecb04838e9ca9f1feba680cb4561c
MaxMEllon <maxmellon1994@gmail.com>
2017-06-27 22:24:48 +0900

    Solved

--- sample/3_16_5.hs
+++ sample/3_16_5.hs
@@ -7,6 +7,18 @@ solve n
 
 solve' = unfoldr (\x -> if x == 0 then Nothing else Just (x `mod` 10, x `div` 10))
 
+power x n
+    | n == 0    = 1
+    | even n    = power (x*x) (n `div` 2)
+    | otherwise = x * power x (n - 1)
+
+
+up a b
+  | a == 0 && b == 0 = 0
+  | otherwise = b * 10 + a
+
+rsolve arr = foldr up 0 arr
+
 main = do
     n <- readLn :: IO Int
     putStrLn $ show $ solve' $ toInteger n
