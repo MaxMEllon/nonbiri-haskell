687b8227a165247972f5b433be1d09c6ebed8bc5
MaxMEllon <maxmellon1994@gmail.com>
2017-06-27 22:38:59 +0900

    Add comment

--- sample/3_16_5.hs
+++ sample/3_16_5.hs
@@ -1,5 +1,14 @@
 import Data.List
 
+-- foldr :: (a -> b -> a) -> b -> [a] -> a <- 返却値
+--          ~~~~~~~~~~~~     |     L 配列
+--             演算子        末尾の左辺
+
+-- unfoldr :: (b -> Maybe (a, b)) -> b -> [a] <- 返却値
+--            ~~~~~~~~~~~~~~~~~~~    L 最初の引数
+--             |           |  L 次の処理の引数
+--            関数         L この処理の配列の要素
+
 solve :: Integer -> [Integer]
 solve n
   | n == 0 = []
@@ -11,7 +20,6 @@ power x n
     | n == 0    = 1
     | even n    = power (x*x) (n `div` 2)
     | otherwise = x * power x (n - 1)
-
 
 up a b
   | a == 0 && b == 0 = 0
